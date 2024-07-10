1af0869abd414a7822b12b045b7aabff37356998
kuaner <kuaner@gmail.com>
2024-07-10 17:24:37 +0800

    feat: 通过环境变量配置openai地址

--- translate/openai.go
+++ translate/openai.go
@@ -1,9 +1,28 @@
 package translate
 
 import (
+	"os"
+	"strconv"
+
 	openai "github.com/zijiren233/openai-translator"
 )
 
 func OpenaiTranslate(q, source, target, key string) (result string, err error) {
-	return openai.Translate(q, target, key, openai.WithFrom(source), openai.WithModel("deepseek-chat"), openai.WithUrl("https://api.deepseek.com/v1"), openai.WithTemperature(1.1))
+	opt := []openai.Option{
+		openai.WithFrom(source),
+	}
+	if withURL := os.Getenv("OPENAI_BASE_URL"); withURL != "" {
+		opt = append(opt, openai.WithUrl(os.Getenv("OPENAI_BASE_URL")))
+	}
+	if withModel := os.Getenv("OPENAI_MODEL"); withModel != "" {
+		opt = append(opt, openai.WithModel(os.Getenv("OPENAI_MODEL")))
+	}
+	if withTemperature := os.Getenv("OPENAI_TEMPERATURE"); withTemperature != "" {
+		// convert string to float32
+		temperature, err := strconv.ParseFloat(withTemperature, 32)
+		if err == nil {
+			opt = append(opt, openai.WithTemperature(float32(temperature)))
+		}
+	}
+	return openai.Translate(q, target, key, opt...)
 }
