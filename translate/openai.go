0f831fc7e5507076b759c74b96eede3d0bbaa0ed
kuaner <kuaner@gmail.com>
2024-07-11 00:09:10 +0800

    fix openai

--- translate/openai.go
+++ translate/openai.go
@@ -1,28 +1,50 @@
 package translate
 
 import (
+	"context"
+	"fmt"
 	"os"
 	"strconv"
 
-	openai "github.com/zijiren233/openai-translator"
+	openai "github.com/sashabaranov/go-openai"
 )
 
 func OpenaiTranslate(q, source, target, key string) (result string, err error) {
-	opt := []openai.Option{
-		openai.WithFrom(source),
-	}
+	openaiConf := openai.DefaultConfig(key)
 	if withURL := os.Getenv("OPENAI_BASE_URL"); withURL != "" {
-		opt = append(opt, openai.WithUrl(withURL))
+		openaiConf.BaseURL = withURL
+	}
+	msg := openai.ChatCompletionRequest{
+		Model:       openai.GPT4o,
+		Temperature: 1.0,
+		Messages: []openai.ChatCompletionMessage{
+			{
+				Role:    openai.ChatMessageRoleSystem,
+				Content: "You are a professional, authentic machine translation engine.",
+			},
+			{
+				Role: openai.ChatMessageRoleUser,
+				Content: fmt.Sprintf(`Translate the following source text to Simplified Chinese Language, Output translation directly without any additional text.
+Source Text: %s`, q),
+			},
+		},
 	}
 	if withModel := os.Getenv("OPENAI_MODEL"); withModel != "" {
-		opt = append(opt, openai.WithModel(withModel))
+		msg.Model = withModel
 	}
 	if withTemperature := os.Getenv("OPENAI_TEMPERATURE"); withTemperature != "" {
 		// convert string to float32
 		temperature, err := strconv.ParseFloat(withTemperature, 32)
 		if err == nil {
-			opt = append(opt, openai.WithTemperature(float32(temperature)))
+			msg.Temperature = float32(temperature)
 		}
 	}
-	return openai.Translate(q, target, key, opt...)
+	resp, err := openai.NewClientWithConfig(openaiConf).CreateChatCompletion(
+		context.Background(),
+		msg,
+	)
+	if err != nil {
+		return "error", err
+	}
+	return resp.Choices[0].Message.Content, nil
 }
